fa6d8e22cdeba54931f17f26966d6d9f708cd3aa
hsuntarq <hsuntariq@gmail.com>
2023-04-23 18:20:47 +0500

    task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/test-tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from './Tasks';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'this is a first task',
+    day: '4th July 2023',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'this is a second task',
+    day: '4th July 2023',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'this is a third task',
+    day: '4th July 2023',
+    reminder: true,
+  },
+];
