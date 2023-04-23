c0fa9c5e79eaad6f65538ea3b02720dc2cf9c20d
hsuntarq <hsuntariq@gmail.com>
2023-04-23 18:19:12 +0500

    tasks service

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { TASKS } from '../test-tasks';
+import { Task } from '../Tasks';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const headerOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private api_url = 'http://localhost:5000/tasks/';
+  constructor(private http: HttpClient) {}
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.api_url);
+  }
+  deleteTask(task: Task): Observable<Task[]> {
+    const url = this.api_url + task.id;
+    return this.http.delete<Task[]>(url);
+  }
+  toggleTask(task: Task): Observable<Task[]> {
+    const url = this.api_url + task.id;
+    return this.http.put<Task[]>(url, task, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
